f89b9b34711ed852f9f249dd5a643a2292d5a8fd
Medix-Code <11797436+Medix-Code@users.noreply.github.com>
2025-02-05 10:18:02 +0100

    fix

--- src/ui/theme.js
+++ src/ui/theme.js
@@ -1,21 +1,39 @@
 // theme.js
-// Detecta si el usuario prefiere el modo oscuro
+// Seleccionem el meta tag theme-color
+const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+
+// Funció per actualitzar el theme-color segons el tema
+function updateThemeColor(isDark) {
+  if (isDark) {
+    // Canvia aquest valor pel color que vulguis per al mode fosc
+    metaThemeColor.setAttribute("content", "#000000");
+  } else {
+    // Canvia aquest valor pel color que vulguis per al mode clar
+    metaThemeColor.setAttribute("content", "#ffffff");
+  }
+}
+
+// Detecta si l'usuari prefereix el mode fosc
 if (
   window.matchMedia &&
   window.matchMedia("(prefers-color-scheme: dark)").matches
 ) {
   document.body.classList.add("theme-dark");
+  updateThemeColor(true);
 } else {
   document.body.classList.remove("theme-dark");
+  updateThemeColor(false);
 }
 
-// Escuchar cambios en la preferencia del usuario en tiempo real
+// Escolta els canvis en la preferència del mode en temps real
 window
   .matchMedia("(prefers-color-scheme: dark)")
   .addEventListener("change", (event) => {
     if (event.matches) {
       document.body.classList.add("theme-dark");
+      updateThemeColor(true);
     } else {
       document.body.classList.remove("theme-dark");
+      updateThemeColor(false);
     }
   });
