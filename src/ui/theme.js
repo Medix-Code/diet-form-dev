91d9b1debce84649a43eea2464a6469683713714
Medix-Code <11797436+Medix-Code@users.noreply.github.com>
2025-02-05 11:45:24 +0100

    fix

--- src/ui/theme.js
+++ src/ui/theme.js
@@ -9,7 +9,7 @@ function updateThemeColor(isDark) {
     metaThemeColor.setAttribute("content", "#343a40");
   } else {
     // Canvia aquest valor pel color que vulguis per al mode clar
-    metaThemeColor.setAttribute("content", "#0056b3");
+    metaThemeColor.setAttribute("content", "#1470c2");
   }
 }
 
