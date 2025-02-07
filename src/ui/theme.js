2fd83e56fa402b8d656dae702e55e115bb7edca7
Medix-Code <11797436+Medix-Code@users.noreply.github.com>
2025-02-07 17:26:18 +0100

    f

--- src/ui/theme.js
+++ src/ui/theme.js
@@ -5,45 +5,47 @@ document.addEventListener("DOMContentLoaded", function () {
   const themeColorMeta = document.querySelector('meta[name="theme-color"]');
 
   // Colors per als dos modes
-  const lightThemeColor = "#004aad"; // Fons per al tema clar
-  const darkThemeColor = "#343a40"; // Fons per al tema fosc
+  const lightThemeColor = "#004aad"; // Color de fons del tema clar
+  const darkThemeColor = "#343a40"; // Color de fons del tema fosc
 
-  // Funció per establir el tema i actualitzar la imatge de l'icona
+  // Funció per establir el tema i actualitzar l'icona i el meta theme-color
   function setTheme(theme) {
     if (theme === "dark") {
       body.classList.add("theme-dark");
       localStorage.setItem("theme", "dark");
-      // Canvia la imatge a la del sol per indicar que està en mode fosc
+      // Canvia la icona al sol per indicar que estem en mode fosc
       themeIcon.src = "assets/icons/sun.svg";
-      if (themeColorMeta)
+      if (themeColorMeta) {
         themeColorMeta.setAttribute("content", darkThemeColor);
+      }
     } else {
       body.classList.remove("theme-dark");
       localStorage.setItem("theme", "light");
-      // Canvia la imatge a la de la lluna per indicar que està en mode clar
+      // Canvia la icona a la lluna per indicar que estem en mode clar
       themeIcon.src = "assets/icons/moon.svg";
-      if (themeColorMeta)
+      if (themeColorMeta) {
         themeColorMeta.setAttribute("content", lightThemeColor);
+      }
     }
   }
 
-  // Comprovar si hi ha un tema guardat en el localStorage
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme) {
-    setTheme(savedTheme);
-  } else {
-    // Si no hi ha tema guardat, detectem la preferència del sistema
-    const prefersDark =
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
-    setTheme(prefersDark ? "dark" : "light");
+  // Comprova si hi ha una preferència guardada a localStorage
+  let savedTheme = localStorage.getItem("theme");
+  if (!savedTheme) {
+    // Si no hi ha preferència guardada, detecta la preferència del sistema
+    savedTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
   }
+  // Estableix el tema segons la preferència trobada
+  setTheme(savedTheme);
 
-  // Afegir event listener per canviar el tema quan es clica el botó
+  // Canvia el tema quan es clica el botó
   themeToggleBtn.addEventListener("click", function () {
     const currentTheme = body.classList.contains("theme-dark")
       ? "dark"
       : "light";
+    // Canvia al tema contrari
     setTheme(currentTheme === "dark" ? "light" : "dark");
   });
 });
