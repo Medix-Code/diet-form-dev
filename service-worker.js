9e846cf753d6f368cb55178efb2cdbf628c7ecb1
Medix-Code <Medix-Code@users.noreply.github.com>
2025-02-09 22:17:25 +0000

    deploy: d5f7ccc409c65668220e73539bd96c13b4842c3c

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Definim el nom del caché i els fitxers a "cachejar"
-const CACHE_NAME = "dieta-cache-v20250209221404"";
+const CACHE_NAME = "dieta-cache-v20250209221724"";
 const urlsToCache = [
   "./",
   "./index.html",
