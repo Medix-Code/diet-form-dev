9d46300d4aba3fdc5ac09bbc9691e25018575151
Medix-Code <Medix-Code@users.noreply.github.com>
2025-03-06 21:38:57 +0000

    deploy: 931445393b2903dc1ece267ab04dcbad9c60bed5

--- src/services/cameraOcr.js
+++ src/services/cameraOcr.js
@@ -41,7 +41,7 @@ export function initCameraOcr() {
           "error"
         );
       } else {
-        showToast("❌ Error desconegut en accedir a la càmera", "error");
+        //   showToast("❌ Error desconegut en accedir a la càmera", "error");
       }
     });
 
