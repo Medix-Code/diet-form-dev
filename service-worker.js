c13f7380c62dec9edd32dd6453b273e0a9320b25
Medix-Code <Medix-Code@users.noreply.github.com>
2025-03-07 14:01:17 +0000

    deploy: 99c32073bb1ee9e64cc5bbba77e41c035249b982

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Definim el nom del caché i els fitxers a "cachejar"
-const CACHE_NAME = "dieta-cache-v20250307135600";
+const CACHE_NAME = "dieta-cache-v20250307140116";
 
 // NOTA: NO incloem index.html ni 404.html en el pre-cache
 // per evitar que es quedin 'encallats' en cache-first.
